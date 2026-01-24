4c6fccfa493a0288fe1828d34e7627d520ba8cb2
MohammadAshrawiSAmporna <mohammadashrawi1011@gmail.com>
2026-01-24 22:50:31 +0800

    s6
    Fixed syntax errors and updated Person object initialization.

---
+++ SeatworkSixThird.java
@@ -0,0 +1,31 @@
+class Person {
+    String name;
+    String section;
+
+    public void Person(String name, String section) {
+        this.name = name;
+        this.section = section;
+    }
+
+    void displayInfo() {
+        System.out.println("Student name: " + name);
+        System.out.println("Section: " + section);
+    }
+}
+
+public class SeatworkSixThird {
+    public static void main(String[] args) {
+        Person p1 = null;
+      Person p2 = new Person():
+
+        Person p1 = new Person();
+        p1.Person("Techpro");
+      p2.Person("hihi"):
+
+        try {
+            p1.displayInfo();
+        } catch (Exception e) {
+            System.out.println("Boy may error");
+        }
+    }
+}
